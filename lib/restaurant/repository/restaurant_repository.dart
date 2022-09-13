f5c65d30752821ddab9525cfc450c46a94969630
3602ghddpwl <bigdatachobo@gmail.com>
2022-09-14 01:45:35 +0900

    riverpod

--- lib/restaurant/repository/restaurant_repository.dart
+++ lib/restaurant/repository/restaurant_repository.dart
@@ -1,10 +1,22 @@
 import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_lv2_course/common/const/data.dart';
+import 'package:flutter_lv2_course/common/dio/dio.dart';
 import 'package:flutter_lv2_course/common/model/cursor_pagination_model.dart';
 import 'package:flutter_lv2_course/restaurant/model/restaurant_detail_model.dart';
 import 'package:flutter_lv2_course/restaurant/model/restaurant_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/http.dart';
 
 part 'restaurant_repository.g.dart';
+
+final restaurantRepositoryProvider = Provider<RestaurantRepository>((ref) {
+  final dio = ref.watch(dioProvider);
+
+  final repository =
+      RestaurantRepository(dio, baseUrl: 'https://$ip/restaurant');
+
+  return repository;
+});
 
 @RestApi()
 abstract class RestaurantRepository {
